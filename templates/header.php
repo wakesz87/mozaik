e796177788984e960b3433263a9503e291601443
Kiss Szabolcs <kisszab87@gmail.com>
2022-01-08 02:27:59 +0100

    first commit

---
+++ templates/header.php
@@ -0,0 +1,12 @@
+<div class=logo>
+    <img src="images\logo.jpg" alt="logo">
+</div>
+<div class="navbar">
+  <a class="active" href="#"><i class="fa fa-fw fa-home"></i> Főoldal</a> 
+  <a href="#"><i class="fa fa-fw fa-search"></i> Katalógus</a> 
+  <a href="#"><i class="fa fa-fw fa-envelope"></i> digitális</a> 
+  <a href="#"><i class="fa fa-fw fa-user"></i> játék</a>
+  <a href="#"><i class="fa fa-fw fa-bell"></i> pedagógus</a>
+  <a href="#"><i class="fa fa-fw fa-comment"></i> infó</a>
+  <a href="#"><i class="fa fa-fw fa-folder"></i> tanár</a>
+</div>
---
+++ templates/layout.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@100&display=swap');
+    </style>
+    <link rel="stylesheet" href="css\style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <title>Mozaik</title>
+</head>
+
+<body>
+
+    <div class="classic">
+        <?php
+        require_once "header.php";
+        require_once "content.php";
+        require_once "footer.php";
+        ?>
+    </div>
+
+</body>
+
+</html>
